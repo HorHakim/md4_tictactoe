91470083662e7f1247a0580d2d0a897bf42e71a6
Hakim Horairy <horairyhakim@gmail.com>
2024-03-08 11:09:03 +0100

    adding minmax algo

--- tictactoe_controller.py
+++ tictactoe_controller.py
@@ -11,6 +11,8 @@ class TicTacToeController:
 		self.tictactoe_model_object = TicTacToeModel()
 		self.connect_widgets()
 
+		self.robot_update_grid()
+
 
 	def connect_widgets(self):
 		for row in range(0, 3):
@@ -20,5 +22,21 @@ class TicTacToeController:
 
 	def update_game(self, row, col):
 		current_turn = self.tictactoe_model_object.turn
-		self.tictactoe_model_object.update_grid(row, col)
+		self.tictactoe_model_object.human_move(row, col)
 		self.tictactoe_view_object.check_cell(row, col, current_turn)
+
+		if not self.tictactoe_model_object.game_in_progress:
+			self.tictactoe_view_object.display_game_status(self.tictactoe_model_object.winner)
+			return None
+
+		self.robot_update_grid()
+
+
+	def robot_update_grid(self):
+		current_turn = self.tictactoe_model_object.turn
+		row, col = self.tictactoe_model_object.robot_move()
+		self.tictactoe_view_object.check_cell(row, col, current_turn)
+
+		if not self.tictactoe_model_object.game_in_progress:
+			self.tictactoe_view_object.display_game_status(self.tictactoe_model_object.winner)
+			return None
--- tictactoe_view.py
+++ tictactoe_view.py
@@ -16,3 +16,18 @@ class TicTacToeView(QMainWindow):
 	def check_cell(self, row, col, current_turn):
 		self.findChild(QPushButton, f"pb_{row}_{col}").setIcon(self.symbols[f"player_{current_turn}"])
 		self.findChild(QPushButton, f"pb_{row}_{col}").setEnabled(False)
+
+	def display_game_status(self, winner):
+		# if winner:
+		# 	self.game_state_label.setText(f"le gagnant est le player {1 if winner == 1 else 2}")
+		# else:
+		# 	self.game_state_label.setText("Match nul")
+
+
+		self.game_state_label.setText('winner : 1' if winner == 1 else ('winner : 2' if winner == -1 else 'Match nul'))
+
+		for row in range (0, 3):
+			for col in range (0, 3):
+				self.findChild(QPushButton, f"pb_{row}_{col}").setEnabled(False)
+
+
